96a98fa7ccbb73b34b6545da0f41d40150a26c3d
mohan7401647399 <mtm.kcs@gmail.com>
2024-12-21 22:56:04 +0530

    tailwind added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,7 @@ import daisyUIThemes from "daisyui/src/theming/themes"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html", './public/index.html'],
   theme: {
     extend: {},
   },
