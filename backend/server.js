1b48ce03bc00346a1a047ea50294c33ae0441f47
mohan7401647399 <mtm.kcs@gmail.com>
2024-12-22 11:34:07 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -40,11 +40,9 @@ app.use("/api/auth", authRoute)                         //  auth route
     .use("/api/notifications", notificationRoute)        //  notification route
 
 if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static(path.join(__dirname, '/frontend/build')))
-    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
     app.use("*", (req, res) => {
-        // res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html'))
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist','index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html'))
     })
 }
 
