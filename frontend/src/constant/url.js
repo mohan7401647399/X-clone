5b9ac47f17cb0845e6c584acbdc22a29d419e518
mohan7401647399 <mtm.kcs@gmail.com>
2024-12-22 09:04:37 +0530

    Live URL changed

--- frontend/src/constant/url.js
+++ frontend/src/constant/url.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5000";
-// export const BASE_URL = "https://twitter-clone-mern-j4di.onrender.com";
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://twitter-clone-mern-j4di.onrender.com";
