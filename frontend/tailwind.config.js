341762cdc6a4e65d462147516d5a5ed02ae262a4
mohan7401647399 <mtm.kcs@gmail.com>
2024-12-23 07:04:53 +0530

    done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,39 +1,33 @@
-import daisyui from "daisyui"
-import daisyUIThemes from "daisyui/src/theming/themes"
-
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
+
 module.exports = {
-//  content: [
- //   "./src/**/*.{js,jsx,ts,tsx,html}",       // Adjust this path based on your project structure
- //   "./public/index.html",
- //   "./build/index.html",
- // ],
-  purge: {
-    content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
-    safelist: [
-      'bg-blue-500',
-      'text-center',
-      'hover:opacity-100',
-      // ...
-      'lg:text-right',
-    ]
-  },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}", // Adjust this path based on your project structure
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [daisyui],
-
+  safelist: [
+    "bg-blue-500",
+    "text-center",
+    "hover:opacity-100",
+    "lg:text-right",
+    // Add other classes as needed
+  ],
   daisyui: {
     themes: [
-      'light',
+      "light",
       {
         black: {
           ...daisyUIThemes["black"],
           primary: "rgb(29, 155, 240)",
-          secondary: "rgb(24, 24, 24)"
-        }
-      }
-    ]
-
-  }
-}
+          secondary: "rgb(24, 24, 24)",
+        },
+      },
+    ],
+  },
+};
